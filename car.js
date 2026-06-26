6ae5de25b74dcca15698ac0fb37880f945f56336
Aditya Rajpoot <adityarajpoot1506@gmail.com>
2026-06-26 17:57:21 +0530

    Update car.js

--- car.js
+++ car.js
@@ -142,7 +142,7 @@ class Car{
         if(this.sensor && drawSensor){
             this.sensor.draw(ctx);
         }
-
+        
         ctx.save();
         ctx.translate(this.x,this.y);
         ctx.rotate(-this.angle);
@@ -154,12 +154,12 @@ class Car{
                 this.height);
             ctx.globalCompositeOperation="multiply";
         }
+        
         ctx.drawImage(this.img,
             -this.width/2,
             -this.height/2,
             this.width,
             this.height);
         ctx.restore();
-
     }
 }
