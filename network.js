979614ae61bd4d4fff3fe41ad16c3b198efeabe2
Aditya Rajpoot <adityarajpoot1506@gmail.com>
2026-07-02 23:13:04 +0530

    Update network.js

--- network.js
+++ network.js
@@ -50,10 +50,8 @@ class Level{
         for(let i=0;i<inputCount;i++){
             this.weights[i]=new Array(outputCount);
         }
-
         Level.#randomize(this);
     }
-
     static #randomize(level){
         for(let i=0;i<level.inputs.length;i++){
             for(let j=0;j<level.outputs.length;j++){
